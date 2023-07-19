28cd1c3d13c68b3e4c055c9088028c89c5670b08
merrittz22 <morritzngueguim43@gmail.com>
2023-07-19 18:23:09 +0200

    index

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Nettoyage professionnel", "Nettoyage de bureau", "Nettoyage evenementiel", "Nettoyage hebdomadaire"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
